0f7669eb6693932530921d005743338e494c3316
math92n4 <math92n4@stud.kea.dk>
2023-02-12 12:28:08 +0100

    done

---
+++ src/main/java/com/example/html_portfolie/HtmlPortfolieApplication.java
@@ -0,0 +1,13 @@
+package com.example.html_portfolie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlPortfolieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmlPortfolieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/html_portfolie/HtmlPortfolieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.html_portfolie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlPortfolieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
